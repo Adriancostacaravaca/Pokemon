c50b3ebf4c73ab6f152ea4ef138041fb1e1049c5
Adrián Costa <Adriancostacaravaca@gmail.com>
2023-03-29 10:42:24 +0200

    Añadido: - Declarados los métodos y botones.

--- src/main/java/org/proyectopokemon/controller/Controladores.java
+++ src/main/java/org/proyectopokemon/controller/Controladores.java
@@ -6,11 +6,47 @@ import javafx.scene.control.Button;
 public class Controladores {
 
     @FXML
+    private Button btnCombate;
+    @FXML
+    private Button btnTienda;
+    @FXML
     private Button btnCaptura;
+    @FXML
+    private Button btnCriarPokemon;
+    @FXML
+    private Button btnMochila;
+    @FXML
+    private Button btnPokedex;
+    @FXML
+    private Button btnSalir;
 
     @FXML
+    public void irAVentanaCombate(){
+        System.out.println("Ir a Combate");
+    }
+    @FXML
+    public void irAVentanaTienda(){
+        System.out.println("Ir a Tienda");
+    }
+    @FXML
     public void irAVentanaCaptura(){
-        System.out.println("Hola");
+        System.out.println("Ir a Captura");
+    }
+    @FXML
+    public void IrAVentanaCrianza(){
+        System.out.println("Ir a Crianza");
+    }
+    @FXML
+    public void irAVentanaMochila(){
+        System.out.println("Ir a Mochila");
+    }
+    @FXML
+    public void irAVentanaPokedex(){
+        System.out.println("Ir a Pokédex");
+    }
+    @FXML
+    public void cerrarPokemon(){
+        System.out.println("Salir");
     }
 
 }
