e9c6d80ff2f918cc9a1f9c86ff1ff0f21881ce65
Adrián Costa <Adriancostacaravaca@gmail.com>
2023-03-29 13:41:01 +0200

    Añadido: - Controlador Crianza - Ventana Crianza

---
+++ src/main/java/org/proyectopokemon/controller/ControladorCrianza.java
@@ -0,0 +1,14 @@
+package org.proyectopokemon.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControladorCrianza {
+    @FXML
+    private Button btnCriar;
+    @FXML
+    public void criarPokemon(){
+        System.out.println("Criar Pokemon");
+    }
+
+}
