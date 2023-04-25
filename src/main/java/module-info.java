36e55a61f52775caf58c4da2350b841b49864edd
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-04-25 20:50:14 +0200

    AÑADIDO: -INTENTO DE CONECTAR LA BASE DE DATOS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Pokemon {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.media;
+    requires java.sql;
 
     opens org.proyectopokemon.model;
     opens org.proyectopokemon.controller;
