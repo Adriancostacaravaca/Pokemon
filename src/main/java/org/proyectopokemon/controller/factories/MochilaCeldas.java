f1775f2ec9c63e5f4729eec1e1b07a1f8767a0c9
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-04-28 00:27:37 +0200

    AÑADIDO: -Presentacion de objetos en la mochila (sin funcionar aun)

---
+++ src/main/java/org/proyectopokemon/controller/factories/MochilaCeldas.java
@@ -0,0 +1,27 @@
+package org.proyectopokemon.controller.factories;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import org.proyectopokemon.model.MovimientoAtaque;
+import org.proyectopokemon.model.Objeto;
+
+public class MochilaCeldas implements Callback<ListView<Objeto>, ListCell<Objeto>> {
+
+    @Override
+    public ListCell<Objeto> call(ListView<Objeto> param) {
+        return new ListCell<Objeto>() {
+            @Override
+            protected void updateItem(Objeto objeto, boolean empty) {
+                super.updateItem(objeto, empty);
+                if (empty || objeto == null) {
+                    setText(null);
+                } else {
+                    setText("Nombre: " + objeto.getNombre() + "\n"
+                            + "Precio: " + objeto.getCoste() + "\n"
+                            + "Descripcion: " + objeto.getDescripcion() + "\n");
+                }
+            }
+        };
+    }
+}
