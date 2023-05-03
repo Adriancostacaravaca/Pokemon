66e392d6132a74341843c1694ee5833db65c7568
Adrián Costa <Adriancostacaravaca@gmail.com>
2023-05-03 12:12:21 +0200

    AÑADIDO: Nueva organización de la Pokédex.

--- src/main/java/org/proyectopokemon/controller/ControladorPokedex.java
+++ src/main/java/org/proyectopokemon/controller/ControladorPokedex.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,6 +25,10 @@ public class ControladorPokedex {
     private Button btnRevisar;
     @FXML
     private Label lblPokedex;
+    @FXML
+    private ListView listViewPokemon;
+    @FXML
+    private ImageView imageViewPokemon;
 
     @FXML
     public void volverAVentanaPrincipal(ActionEvent event) throws IOException {
