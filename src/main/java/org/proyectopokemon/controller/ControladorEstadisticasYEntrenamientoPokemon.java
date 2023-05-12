1e39e4a672e8028dcf90ed75d670e49ae3fac423
Adrian <Adriancostacaravaca@gmail.com>
2023-05-12 13:01:03 +0200

    AÑADIDO: Probando nuevo entrenamiento.

--- src/main/java/org/proyectopokemon/controller/ControladorEstadisticasYEntrenamientoPokemon.java
+++ src/main/java/org/proyectopokemon/controller/ControladorEstadisticasYEntrenamientoPokemon.java
@@ -1,4 +1,11 @@
 package org.proyectopokemon.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ControladorEstadisticasYEntrenamientoPokemon {
+
+    @FXML
+    private Label lblNombre;
+
 }
