77ff4a115ca81840f44d358a77e406abd263aef9
FranciscoEstrada02 <francisco.s295973@cesurformacion.com>
2023-05-12 14:09:29 +0200

    AÑADIDO: -Movimientos de todos los tipos

--- src/main/java/org/proyectopokemon/model/MovimientosDisponiblesParaPokemon.java
+++ src/main/java/org/proyectopokemon/model/MovimientosDisponiblesParaPokemon.java
@@ -16,6 +16,20 @@ public class MovimientosDisponiblesParaPokemon {
         movimientos.add(new MovimientoAtaque("Chispa",1,13,Tipo.ELECTRICO));
         movimientos.add(new MovimientoAtaque("Chispazo",1,20,Tipo.ELECTRICO));
         movimientos.add(new MovimientoAtaque("Placaje",1,20,Tipo.ELECTRICO));
+        movimientos.add(new MovimientoAtaque("Placaje",1,20,Tipo.ELECTRICO));
+
+        movimientos.add(new MovimientoEstado("Congelar",1, Estado.CONGELADO));
+        movimientos.add(new MovimientoEstado("Maldecir",1,Estado.MALDITO));
+        movimientos.add(new MovimientoEstado("Enamorar",1,Estado.ENAMORADO));
+        movimientos.add(new MovimientoEstado("Dormir",1,Estado.DORMIDO));
+        movimientos.add(new MovimientoEstado("Atrapado",1,Estado.ATRAPADO));
+
+        movimientos.add(new MovimientoMejora("Mejora1",1));
+        movimientos.add(new MovimientoMejora("Mejora2",1));
+        movimientos.add(new MovimientoMejora("Mejora3",1));
+        movimientos.add(new MovimientoMejora("Mejora4",1));
+        movimientos.add(new MovimientoMejora("Mejora5",1));
+
     }
 
 
